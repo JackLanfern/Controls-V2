e6e8e64ce8f1a6571e94f9be61be12851223f227
JackLanfern <83324108+JackLanfern@users.noreply.github.com>
2024-10-29 17:31:55 -0400

    Added test-git file.

---
+++ test-git-blink/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+// put function declarations here:
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(23, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Hello ");
+  digitalWrite(23, HIGH);
+  delay(500);
+  Serial.println("World");
+  digitalWrite(23, LOW);
+  delay(500);
+}
